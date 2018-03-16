62d3619da2d93ce6da5da72ac65121f2cf1f0add
Sune Simonsen <sune@we-knowhow.dk>
2018-03-16 22:23:21 +0100

    Transpile the distribution files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactDom from "react-dom";
+import TestUtils from "react-dom/test-utils";
+
+class StatelessWrapper extends Component {
+  render() {
+    return this.props.children;
+  }
+}
+
+StatelessWrapper.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+export function mount(component) {
+  const rendered =
+    TestUtils.renderIntoDocument(component) ||
+    TestUtils.renderIntoDocument(
+      <StatelessWrapper>{component}</StatelessWrapper>
+    );
+
+  return ReactDom.findDOMNode(rendered);
+}
