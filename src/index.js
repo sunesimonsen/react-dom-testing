83ac9d7c57fd442e6d82bd77820046762777e9e8
Sune Simonsen <sune@we-knowhow.dk>
2018-03-17 22:52:59 +0100

    Fixed linting errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
 import ReactDom from "react-dom";
-import TestUtils from "react-dom/test-utils";
 
 export function mount(element) {
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   ReactDom.render(element, div);
   return div.firstChild;
 }
