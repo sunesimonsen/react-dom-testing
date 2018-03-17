2dd9683bb5804e53515507df0102baa305eb7a69
Sune Simonsen <sune@we-knowhow.dk>
2018-03-17 11:06:21 +0100

    Removed dependency on prop-types

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import ReactDom from "react-dom";
 import TestUtils from "react-dom/test-utils";
 
 class StatelessWrapper extends Component {
   render() {
-    return this.props.children;
+    return this.props.children; // eslint-disable-line react/prop-types
   }
 }
-
-StatelessWrapper.propTypes = {
-  children: PropTypes.element.isRequired
-};
 
 export function mount(component) {
   const rendered =
